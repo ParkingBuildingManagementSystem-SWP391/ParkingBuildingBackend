2b6f187641c64f0698a631fa499cfaccf7f4191f
HienVinh-ne <thv15062005@gmail.com>
2026-05-29 20:00:37 +0700

    basic setup for the coding booking

--- ParkingBuilding.Service/obj/Debug/net8.0/ParkingBuilding.Service.AssemblyInfo.cs
+++ ParkingBuilding.Service/obj/Debug/net8.0/ParkingBuilding.Service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingBuilding.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93bb654f56b0f83eecfeff05e33cc29817b9d333")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1afeddf9e2209b802baf93b0cdb974f234c7fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingBuilding.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingBuilding.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
